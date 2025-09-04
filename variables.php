ba023a4442d60e5dd3c011a3a35fa61e3a200333
helyipapa <milanmorzsa18@gmail.com>
2025-09-04 10:52:16 +0200

    elokeszites

---
+++ variables.php
@@ -0,0 +1,13 @@
+<?php
+    /*
+    szükséges programok:XAMP(Apache,MySQL),Git,github,repo,VSC
+    <?php ... ?>
+    Változok
+    */
+    $name = "helyipapa";
+    $age = 18;
+    $city = "Törökszentmiklós";
+    var_dump($name);
+
+    echo "<br>Név: $name Életkor: $age Lakóhely: $city";
+?>
